410f9e1a30736ea77af19bf4710e26b271a7f0cb
Lysak-AA <frigh701@gmail.com>
2023-10-27 12:33:39 +1000

    set up memberstack

---
+++ src/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+  <header class="header">
+    <slot></slot>
+  </header>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+  .header {
+    width: 100%;
+    height: 100px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-bottom: 1px solid grey;
+  }
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,27 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+const openModal = (type) => {
+  window.$memberstackDom.openModal(type)
+}
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
+  <main class="home">
+    <h1>Home Page</h1>
+    <button data-ms-modal="signup" @click="openModal('SIGNUP')">Sign Up</button>
+    <br>
+    <button data-ms-modal="login" @click="openModal('LOGIN')">Log in</button>
+    <br>
+    <a href="" data-ms-modal="signup">Sign Up Link</a>
   </main>
 </template>
+
+<style scoped>
+.home {
+  width: 100%;
+  height: 80vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+</style>
