df5b84873527fdbaff6f5339d5a4924429c7b66e
Lysak-AA <frigh701@gmail.com>
2023-10-27 13:00:56 +1000

    add sign up page

--- src/router/index.js
+++ src/router/index.js
@@ -24,6 +24,14 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/SecretView.vue')
+    },
+    {
+      path: '/sign-up',
+      name: 'sign-up',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/SignUpView.vue')
     }
   ]
 })
---
+++ src/views/SignUpView.vue
@@ -0,0 +1,32 @@
+<script setup>
+const openModal = (type) => {
+  window.$memberstackDom.openModal(type)
+}
+</script>
+
+<template>
+  <main class="home">
+    <h1>Please Sign Up</h1>
+    <span>Those buttons below invoke pre build ms modal without using ms attributes. But they have to use js methods</span>
+    <button @click="openModal('SIGNUP')">Sign Up</button>
+    <br>
+    <button @click="openModal('LOGIN')">Log in</button>
+    <br>
+    <span>This is a link that invokes pre build ms modals using ms attributes: <a data-ms-modal="signup" class="m-link">Sign Up Link</a></span>
+  </main>
+</template>
+
+<style scoped>
+.home {
+  width: 100%;
+  height: 80vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+.m-link {
+  color: blue;
+  cursor: pointer;
+}
+</style>
